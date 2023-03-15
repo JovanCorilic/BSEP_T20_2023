28c8dc9fd5c463f6c3d8213b7fde8288a536c1a4
JovanCorilic <kssbc39@gmail.com>
2023-03-15 09:28:47 +0100

    Postavljanje Admin aplikacije

---
+++ AdminBackend/src/test/java/Backend/AdminBackend/AdminBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package Backend.AdminBackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdminBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
